af2bfcbec312c05422c46ec0c5c0a93b063fa9f1
Claudiomildo Ventura <claudiomildo@hotmail.com>
2025-12-12 22:16:44 -0300

    Enhance UI components

--- src/app/components/structure-view/page-structure/page-structure.component.ts
+++ src/app/components/structure-view/page-structure/page-structure.component.ts
@@ -8,11 +8,13 @@ import {CommonModule} from "@angular/common";
 import {MatTable, MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {MatSort} from "@angular/material/sort";
 import {PageTitleComponent} from "../../dashboard-view/page-title/page-title.component";
+import {SelectionModel} from "@angular/cdk/collections";
+import {MatCheckbox} from "@angular/material/checkbox";
 
 @Component({
     selector: 'page-structure',
     standalone: true,
-    imports: [CommonModule, MatTable, MatTableModule, PageTitleComponent],
+    imports: [CommonModule, MatTable, MatTableModule, PageTitleComponent, MatCheckbox],
     templateUrl: './page-structure.component.html',
     styleUrl: './archetype-structure-app.component.css'
 })
@@ -24,8 +26,12 @@ export class PageStructureComponent implements OnInit, AfterViewInit {
     private readonly archetypeService: ArchetypeService = inject(ArchetypeService);
     private detailContent: unknown;
     public tableData: any[] = [];
-    public dataSource = new MatTableDataSource<any>();
+    public dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
     displayedColumns: string[] = ['id', 'name', 'type', 'length'];
+    displayedColumnsWithSelect: string[] = ['select', ...this.displayedColumns];
+
+    selection = new SelectionModel<any>(true, []); // true = multiple selection
+
     ngOnInit(): void {
         const {detailContent} = history.state ?? {};
         this.detailContent = detailContent;
@@ -35,6 +41,24 @@ export class PageStructureComponent implements OnInit, AfterViewInit {
         this.dataPost(this.detailContent as string);
         //this.dataSource.sort = this.sort;
     }
+
+    /** Whether the number of selected elements matches the total number of rows. */
+    public isAllSelected() {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.dataSource.data.length;
+        return numSelected === numRows;
+    }
+
+    /** Selects all rows if they are not all selected; otherwise clear selection. */
+    public masterToggle() {
+        this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+
+    /** Toggle a single row */
+    public toggleRow(row: any) {
+        this.selection.toggle(row);
+    }
+
 
     private async dataPost(data: string): Promise<void> {
         const metadata = {data: data};
